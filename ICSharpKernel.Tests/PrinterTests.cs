e5d9b7088f412a8a921e228354a09ed35927c2e2
Krisztian Gyuris <gyurisc@gmail.com>
2017-05-31 09:19:22 +0200

    Tests for printer

--- ICSharpKernel.Tests/PrinterTests.cs
+++ ICSharpKernel.Tests/PrinterTests.cs
@@ -14,7 +14,47 @@ namespace ICSharpKernel.Tests
             var result = Printers.PrintVariable(numbers);
 
             Assert.IsInstanceOfType(result, typeof(BinaryOutput));
-            Assert.Equals(result.ContentType, "text/plain");
+            Assert.AreEqual(result.ContentType, "text/plain");
+            Assert.AreEqual("List<int>(6) { 1, 2, 3, 4, 5, 6 }", result.Data);
+        }
+
+        [TestMethod]
+        public void TesPrintingInt()
+        {
+            var number = 8;
+            var result = Printers.PrintVariable(number);
+
+            Assert.IsInstanceOfType(result, typeof(BinaryOutput));
+            Assert.AreEqual(result.ContentType, "text/plain");
+            Assert.AreEqual("8", result.Data);
+        }
+
+        [TestMethod]
+        public void TesPrintingString()
+        {
+            string text = "Hello";
+            var result = Printers.PrintVariable(text);
+
+            Assert.IsInstanceOfType(result, typeof(BinaryOutput));
+            Assert.AreEqual(result.ContentType, "text/plain");
+            Assert.AreEqual("\"Hello\"", result.Data);
+        }
+
+
+        public class SimpleClass
+        {
+            public string TextField { get; set; }
+        }
+
+        [TestMethod]
+        public void TestRemovalOfSubmission()
+        {
+            SimpleClass s = new SimpleClass() { TextField = "Simple" };
+
+            var result = Printers.PrintVariable(s);
+            Assert.IsInstanceOfType(result, typeof(BinaryOutput));
+            Assert.AreEqual(result.ContentType, "text/plain");
+            Assert.AreEqual("PrinterTests.SimpleClass { TextField=\"Simple\" }", result.Data);
         }
     }
 }
