48dd0f46a4dc72f7eb65e2cc65ea24841a9f1135
Krisztian Gyuris <gyurisc@gmail.com>
2017-05-31 08:41:45 +0200

    Adding tests

---
+++ ICSharpKernel.Tests/PrinterTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using ICSharp.Kernel;
+
+namespace ICSharpKernel.Tests
+{
+    [TestClass]
+    public class PrinterTests
+    {
+        [TestMethod]
+        public void TestPrintingSimpleList()
+        {
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6};
+            var result = Printers.PrintVariable(numbers);
+
+            Assert.IsInstanceOfType(result, typeof(BinaryOutput));
+            Assert.Equals(result.ContentType, "text/plain");
+        }
+    }
+}
---
+++ ICSharpKernel.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ICSharpKernel.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ICSharpKernel.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("eb15bb92-adbb-445f-aff9-d8f04711c123")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
