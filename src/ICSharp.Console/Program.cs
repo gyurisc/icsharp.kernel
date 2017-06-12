df599a7f9307f88ebb8aed0273464b4038a56136
Krisztian Gyuris <gyurisc@gmail.com>
2017-06-12 07:52:41 +0200

    removing plotly code

--- src/ICSharp.Console/Program.cs
+++ src/ICSharp.Console/Program.cs
@@ -3,8 +3,6 @@ using System;
 using System.Globalization;
 using System.IO;
 using Trinet.Core.IO.Ntfs;
-using XPlot.Plotly;
-using static XPlot.Plotly.Graph;
 
 namespace ICSharp.Console
 {
@@ -24,7 +22,6 @@ namespace ICSharp.Console
 
         static void Main(string[] args)
         {
-            PltlyTest();
             //On Windows if you download our zip releases you files will be marked as from the Internet
             //Depending on how you extract the files this marker may be left, this will break Paket, so clear it.
             if (Environment.OSVersion.Platform != PlatformID.Unix && Environment.OSVersion.Platform != PlatformID.MacOSX)
@@ -34,18 +31,6 @@ namespace ICSharp.Console
             CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
             CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
             App.Start(args);
-        }
-
-        public static void PltlyTest()
-        {
-            Heatmap h = new Heatmap();
-            h.z = new int[,] { { 1, 20, 30 }, { 20, 1, 60 }, { 30, 60, 1 } };
-            var chart = Chart.Plot(h);
-            chart.WithLayout(new Layout.Layout() { title = "Heatmap example!" });
-            chart.WithHeight(500);
-            chart.WithWidth(700);
-
-            string html = chart.GetInlineHtml();
         }
     }
 }
