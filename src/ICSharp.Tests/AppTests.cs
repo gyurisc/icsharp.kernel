d27ad74de63c54cb522124eda302e2bc609288b2
Krisztian Gyuris <gyurisc@gmail.com>
2017-07-18 10:29:11 +0200

    Adding empty apptests. it is empty for now

---
+++ src/ICSharp.Tests/AppTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using ICSharp.Kernel;
+
+
+namespace ICSharp.Tests
+{
+    [TestClass]
+    public class AppTests
+    {
+       
+    }
+}
